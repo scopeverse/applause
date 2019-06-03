d58a4a6f88147f6cc35e8c68cd62b0c86e3679c7
Malik Piara <malikpiara@gmail.com>
2019-06-03 11:53:16 +0200

    Updates

---
+++ precache-manifest.a50f49544fbca674f952547a62ff6c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc99bfbe0f1fca95d366d10a9a1203a",
+    "url": "/applause/index.html"
+  },
+  {
+    "revision": "773ce43c984c6d9f3f36",
+    "url": "/applause/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/applause/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "773ce43c984c6d9f3f36",
+    "url": "/applause/static/js/main.915e9e1d.chunk.js"
+  },
+  {
+    "revision": "d750d012e752be9ab23d",
+    "url": "/applause/static/js/runtime~main.56210304.js"
+  }
+]);
