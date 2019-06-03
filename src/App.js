9960b003040f4ec5f4609d5df141fd1585ca40c8
Malik Piara <malikpiara@gmail.com>
2019-06-03 11:02:10 +0200

    Created my first react app, started working on the applause component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Applause extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      clapsCount: 0,
+    };
+    this.clap = this.clap.bind(this);
+  }
+  clap(num) {
+    // if button is clicked change state of clapsCount by incrementing it
+    num = this.state.clapsCount + 1
+    this.setState({ clapsCount: num});
+  }
+  render () {
+    return (
+    <div>
+      <h1>Number of Claps: {this.state.clapsCount}</h1>
+      <button onClick={this.clap}>
+        Click Here to Clap
+      </button>
+    </div>
+    )
+  }
+}
+
+// if the person clicks there, increment the clapsCount
+// button.onClick 
+// this.setState({clapsCount++});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Applause />
       </header>
     </div>
   );
